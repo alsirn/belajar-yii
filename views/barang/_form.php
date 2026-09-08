6d3dbdd157393d4f4ad24350474c59d53e6f9887
Alsirann <zahrangren78@gmail.com>
2026-09-08 21:56:31 +0700

    Crud
    membuat crud untuk model barang

---
+++ views/barang/_form.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/** @var yii\web\View $this */
+/** @var app\models\Barang $model */
+/** @var yii\widgets\ActiveForm $form */
+?>
+
+<div class="barang-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'id')->textInput() ?>
+
+    <?= $form->field($model, 'nama')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'stock')->textInput() ?>
+
+    <?= $form->field($model, 'harga')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
